8e652c5dfee61acbff31f1e00038111748c3b80e
Ben Currie <benjamin.w.currie@gmail.com>
2017-05-03 19:00:20 +0100

    another test from codewars

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Question 1
+// Question 1 - Javascript
 
 Square every digit of a number.
 For example, if we run 9119 through the function, 811181 will come out.
@@ -12,4 +12,22 @@ function squareDigits(num){
     arr.push(x);
   }
   return parseInt(arr.join(''));
+}
+
+
+
+// Question 2
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+function isIsogram(str){
+  const x = str.toLowerCase();
+  for(var i = 0; i <= x.length; i++) {
+    for(var y = i+1; y <= x.length; y++) {
+      if(x[y] == x[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
