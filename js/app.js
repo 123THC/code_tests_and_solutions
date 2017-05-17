d88723867e82f07fdc154b44245db39e35ad9293
Ben Currie <benjamin.w.currie@gmail.com>
2017-05-17 16:44:53 +0100

    added facebook like challenge from codewars

--- js/app.js
+++ js/app.js
@@ -46,8 +46,37 @@ function filter_list(l) {
   return result;
 }
 
-// this is someone else answer - way better!
+// this is someone else's answer - way better!
 
 function filter_list(l) {
   return l.filter(function(v) {return typeof v == 'number'})
+}
+
+
+// Question 4
+
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+//
+// Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+//
+// likes [] // must be "no one likes this"
+// likes ["Peter"] // must be "Peter likes this"
+// likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  let people = names.length;
+  if (people === 0) {
+    return 'no one likes this'
+  } else if (people === 1) {
+    return (names + ' ' + 'likes this');
+  } else if (people === 2) {
+    return (names[0] + ' and ' + names[1] + ' like this');
+  } else if (people === 3) {
+    return (names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this');
+  } else if (people > 3) {
+    let remaining = people - 2;
+    return (names[0] + ', ' + names[1] + ' and ' + remaining + ' others like this');
+  }
 }
