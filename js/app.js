088c375ba325827fce7a4ca77dba59f3fce32f51
Ben Currie <benjamin.w.currie@gmail.com>
2017-05-17 17:51:50 +0100

    added 2 new tests

--- js/app.js
+++ js/app.js
@@ -80,3 +80,53 @@ function likes(names) {
     return (names[0] + ', ' + names[1] + ' and ' + remaining + ' others like this');
   }
 }
+
+// Question 5
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns N.
+//
+// For example:
+//
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+//
+// Should return: 11
+//
+// [160, 3, 1719, 19, 11, 13, -21]
+//
+// Should return: 160
+
+
+// not passing all tests yet
+
+function findOutlier(integers){
+  var sum = integers.reduce(function(acc, val) {
+    return acc + val;
+  }, 0);
+  if (sum % 2 === 0) {
+    for (i=0; i < integers.length; i++) {
+      if (integers[i] % 2 === 0) {
+        return integers[i];
+      }
+    }
+  } else {
+    for (i=0; i < integers.length; i++) {
+      if (integers[i] % 2 !== 0) {
+        return integers[i];
+      }
+    }
+  }
+}
+
+// Question 6
+
+// Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+//
+// each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+// a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
+
+function longest(s1, s2) {
+  const joined = s1.concat(s2);
+  const uniqueJoined = Array.from(new Set(joined));
+  let x = uniqueJoined.sort().join('');
+  return x;
+}
